622f4f7fdea66543004a229a5939adf28de2f18b
Yash-y-ash <wardhan696@gmail.com>
2021-03-21 20:55:52 +0530

    Added: java project

---
+++ src/main/java/com/JavaPractice/App.java
@@ -0,0 +1,23 @@
+package com.JavaPractice;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	private static final Logger LOG = LogManager.getLogger(App.class);
+    public static void main( String[] args )
+    {
+    	String message = "Hello World";
+    	LOG.debug(message + "will Be Printrd On Debug");
+    	LOG.info(message + "will Be Printrd On Info");
+    	LOG.warn(message + "will Be Printrd On Warn");
+    	LOG.error(message + "will Be Printrd On Error");
+    	LOG.fatal(message + "will Be Printrd On Fatal");
+    	LOG.info("Appending string: {}.", message);
+        System.out.println(message);
+    }
+}
